669dfee75b2814236d51639e36d7543c343ed248
3bdulra7manAmir <ethanhunt4d@gmail.com>
2023-07-13 20:50:22 +0300

    first commit

---
+++ lib/shared/components/default_btn.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+Widget defaultButton({
+  required String txt,
+  required double width,
+  //required double height,
+  required Color color,
+  required VoidCallback function,
+
+}) => 
+
+Container(
+  width: width,
+  //height: 10.0,
+  color: color,
+  child: MaterialButton(
+  onPressed: function,
+  child: Text(txt.toUpperCase(),
+  style: const TextStyle(color: Colors.white),
+  ),
+  ),
+);
