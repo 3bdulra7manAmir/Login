d67294361a20d896b0c248e43dcce2a26ee80ccc
3bdulra7manAmir <ethanhunt4d@gmail.com>
2023-07-14 20:43:55 +0300

    Updated

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: must_be_immutable
+// ignore_for_file: must_be_immutable, avoid_print
 
 import 'package:flutter/material.dart';
 import 'package:reusable_components/shared/components/default_btn.dart';
@@ -10,6 +10,7 @@ final emailAddressController = TextEditingController();
 final passwordController = TextEditingController();
 String emailAddress = '';
 String password = '';
+var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +22,7 @@ String password = '';
       child: Center(
         child: SingleChildScrollView(
           child: Form(
+            key: formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -30,8 +32,15 @@ String password = '';
                 enableSuggestions: true,
                 controller: emailAddressController,
                 keyboardType: TextInputType.emailAddress,
-                onFieldSubmitted: (value) {
+                onFieldSubmitted: (value){
                   print(value);
+                },
+                // onChanged: ((value){
+                //   print(value);
+                // }),
+                validator: (value){
+                  if (value != null && value.isEmpty) {return 'email address must not be empty';}
+                  return null;
                 },
                 decoration: const InputDecoration(
                   hintText: "Email Address",
@@ -47,8 +56,15 @@ String password = '';
                 enableSuggestions: true,
                 controller: passwordController,
                 keyboardType: TextInputType.visiblePassword, obscureText: true,
-                onFieldSubmitted: (value) {
-                  print(value);
+                onFieldSubmitted: (value2) {
+                  print(value2);
+                },
+                // onChanged:(value2) {
+                //   print(value2);
+                // },
+                validator: (value2) {
+                  if (value2 != null && value2.isEmpty) {return 'Password must not be empty';}
+                  return null;
                 },
                 decoration: const InputDecoration(
                   hintText: "Password",
@@ -56,17 +72,18 @@ String password = '';
                   prefixIcon: Icon(Icons.lock),
                   suffixIcon: Icon(Icons.remove_red_eye),
                   border: OutlineInputBorder(),
-                ),
-                //decoration: ,          
+                ),        
                 ),
                 const SizedBox(height: 20,),
                 Container( width: double.infinity, height: 45, color: Colors.blue,
-                  child: MaterialButton(onPressed: () {emailAddress = emailAddressController.text; print(emailAddress);}, child: const Text("LOGIN", style: TextStyle(fontWeight: FontWeight.w400, fontSize: 15, color: Colors.black,)), )),
+                  child: MaterialButton(onPressed: () {emailAddress = emailAddressController.text; print(emailAddress); password = passwordController.text; print(password);}, child: const Text("LOGIN", style: TextStyle(fontWeight: FontWeight.w400, fontSize: 15, color: Colors.black,)), )),
                 const SizedBox(height: 10,),
                 Column(children: [
                   defaultButton(
                     color: Colors.red,
-                    function: () {print("here");},
+                    function: (){
+                      if(formKey.currentState!.validate()) {print("error");}
+                    },
                     txt: 'Register',
                     width: double.infinity
                   ),
