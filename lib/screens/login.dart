3d1d7c0dcfd9d158aca75a97431a2205e9c09250
3bdulra7manAmir <ethanhunt4d@gmail.com>
2023-07-14 18:18:11 +0300

    added Register btn using resuable Widget

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
+import 'package:reusable_components/shared/components/default_btn.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
@@ -57,10 +60,21 @@ String password = '';
               const SizedBox(height: 20,),
               Container( width: double.infinity, height: 45, color: Colors.blue,
                 child: MaterialButton(onPressed: () {emailAddress = emailAddressController.text; print(emailAddress);}, child: const Text("LOGIN", style: TextStyle(fontWeight: FontWeight.w400, fontSize: 15, color: Colors.black,)), )),
-              const SizedBox(height: 20,),
+              const SizedBox(height: 10,),
+              Column(children: [
+                defaultButton(
+                  color: Colors.red,
+                  function: () {print("here");},
+                  txt: 'Register',
+                  width: double.infinity
+                ),
+              ],
+              ),
+              const SizedBox(height: 10,),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [const Text("Don't have an account?"), TextButton(onPressed: (){}, child: const Text("Register now!"))],)
+                children: [const Text("Don't have an account?"), TextButton(onPressed: (){}, child: const Text("Register now!"))],
+                ),
             ],
           ),
         ),
